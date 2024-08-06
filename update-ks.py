feb83cc0b4eb0a74372ab8ae6ce33083137e6930
{lucas-f-v} <{lucas.vicenzotto@growth.stackspot.com}>
2024-08-06 11:56:44 -0700

    commit w get bearer token

--- update-ks.py
+++ update-ks.py
@@ -27,7 +27,7 @@ print(response)
 print(response.json)
 upload_data = response.json()
 
-# Print the file upload id
+# Print the file upload idd
 print(f"file upload id = {upload_data['id']}")
 
 # Step 2: Upload the file
