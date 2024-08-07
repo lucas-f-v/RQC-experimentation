e115e05831ac0dcdbdaea3469e071a507e83d22c
{lucas-f-v} <{lucas.vicenzotto@growth.stackspot.com}>
2024-08-07 10:36:42 -0700

    fetching the response from the qc now

--- update-ks.py
+++ update-ks.py
@@ -27,7 +27,7 @@ print(response)
 print(response.json)
 upload_data = response.json()
 
-# Print the file upload iddddddddddddddd
+# Print the file upload idddddddddddddddd
 print(f"file upload id = {upload_data['id']}")
 
 # Step 2: Upload the file
