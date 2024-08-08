09e05c05b99612a1f8acf5328d758fa1fac7f905
{lucas-f-v} <{lucas.vicenzotto@growth.stackspot.com}>
2024-08-08 11:04:10 -0700

    extracting code into a json

--- update-ks.py
+++ update-ks.py
@@ -27,7 +27,7 @@ print(response)
 print(response.json)
 upload_data = response.json()
 
-# Print the file upload idddddddddddddddddddddddddddddddddddddddddddddddd
+# Print the file upload iddddddddddddddddddddddddddddddddddddddddddddddddd
 print(f"file upload id = {upload_data['id']}")
 
 # Step 2: Upload the file
