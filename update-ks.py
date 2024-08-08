7cfb63a5f8797f0d4547379aecf42d3175478197
{lucas-f-v} <{lucas.vicenzotto@growth.stackspot.com}>
2024-08-08 11:26:28 -0700

    prev worked, now pycode in rqc json

--- update-ks.py
+++ update-ks.py
@@ -27,7 +27,7 @@ print(response)
 print(response.json)
 upload_data = response.json()
 
-# Print the file upload iddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# Print the file upload idddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 print(f"file upload id = {upload_data['id']}")
 
 # Step 2: Upload the file
