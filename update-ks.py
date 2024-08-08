a0e707e4f7060fda8f2f59223faa1ccaef3cd9a0
{lucas-f-v} <{lucas.vicenzotto@growth.stackspot.com}>
2024-08-08 10:17:19 -0700

    patch

--- update-ks.py
+++ update-ks.py
@@ -27,7 +27,7 @@ print(response)
 print(response.json)
 upload_data = response.json()
 
-# Print the file upload iddddddddddddddddddddddddddddddddddddddddd
+# Print the file upload idddddddddddddddddddddddddddddddddddddddddd
 print(f"file upload id = {upload_data['id']}")
 
 # Step 2: Upload the file
