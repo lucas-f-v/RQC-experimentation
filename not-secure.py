852abc7bf7398eb14ef83decea7c781a798d6db1
{lucas-f-v} <{lucas.vicenzotto@growth.stackspot.com}>
2024-08-13 13:42:32 -0700

    unsure of result, 2 py commit files

--- not-secure.py
+++ not-secure.py
@@ -27,7 +27,7 @@ print(response)
 print(response.json)
 upload_data = response.json()
 
-# Print the file upload idddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# Print the file upload iddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 print(f"file upload id = {upload_data['id']}")
 
 # Step 2: Upload the file
