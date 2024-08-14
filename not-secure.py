336bdb4b9455c5d9b419352a9509ac28bd683428
{lucas-f-v} <{lucas.vicenzotto@growth.stackspot.com}>
2024-08-14 10:18:39 -0700

    nogo!

--- not-secure.py
+++ not-secure.py
@@ -27,7 +27,7 @@ print(response)
 print(response.json)
 upload_data = response.json()
 
-# Print the file upload iddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# Print the file upload idddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 print(f"file upload id = {upload_data['id']}")
 
 # Step 2: Upload the file
